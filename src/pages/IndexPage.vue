aacaf107f0fcdaff82d9a1714ecc5b726ecf1622
Eze323 <a.ezequielirace@gmail.com>
2023-10-06 23:59:15 -0300

    update

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 600px">
+      <q-chat-message
+        name="me"
+        avatar="https://cdn.quasar.dev/img/avatar1.jpg"
+        :text="['Hola cachorra como estas?? tanto tiempo que no te veia']"
+        stamp="7 minutes ago"
+        sent
+        bg-color="amber-7"
+      />
+      <q-chat-message
+        name="Jane"
+        avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+        :text="['Que haces perdido, todo muy tranquilo. Cuando nos juntamos a charlar, tengo muchas cosas para contarte']"
+        stamp="4 minutes ago"
+        text-color="white"
+        bg-color="primary"
+      />
+      <q-chat-message
+        name="me"
+        avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+        :text="['Hola cachorra como estas?? tanto tiempo que no te veia']"
+        stamp="7 minutes ago"
+        sent
+        bg-color="amber-7"
+      />
+      <q-chat-message
+        name="Jane"
+        avatar="https://cdn.quasar.dev/img/avatar3.jpg"
+        :text="['Que haces perdido, todo muy tranquilo. Cuando nos juntamos a charlar, tengo muchas cosas para contarte']"
+        stamp="4 minutes ago"
+        text-color="black"
+        bg-color="secundary"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
